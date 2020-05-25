35fa1878be63793cb24863d9beaa556b3bcc79a9
Michelle Lock <michelleloc@gmail.com>
2020-05-25 23:38:43 +0800

    Finish all iterations

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+const usersArray = [
+  {
+    firstName: 'Kirby',
+    lastName: 'Doyle',
+    id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
+    isActive: false,
+    balance: '$3,570.06',
+    gender: 'male'
+  },
+  {
+    firstName: 'Tracie',
+    lastName: 'May',
+    id: '1af0e9ee-66fc-4298-b8ce-5d99bcbaac05',
+    isActive: false,
+    balance: '$1,547.73',
+    gender: 'female'
+  },
+  {
+    firstName: 'Kendra',
+    lastName: 'Hines',
+    id: '5e92af3a-b08e-4689-bdeb-3226300470e3',
+    isActive: true,
+    balance: '$12,383.08',
+    gender: 'female'
+  },
+  {
+    firstName: 'Kinney',
+    lastName: 'Howard',
+    id: '0ad2388d-83e1-4831-9cc4-e3581f8edf36',
+    isActive: false,
+    balance: '$3,207.06',
+    gender: 'male'
+  },
+  {
+    firstName: 'Howard',
+    lastName: 'Gilmore',
+    id: '0719205d-c965-44cb-a128-708cf335b26c',
+    isActive: true,
+    balance: '$21,307.75',
+    gender: 'male'
+  },
+  {
+    firstName: 'Rachelle',
+    lastName: 'Schneider',
+    id: '04012184-651b-41eb-9642-d362fedff02f',
+    isActive: true,
+    balance: '$35,121.49',
+    gender: 'female'
+  },
+  {
+    firstName: 'Lizzie',
+    lastName: 'Alford',
+    id: '598ca7ec-888e-494d-ae94-c21ace3ffa52',
+    isActive: false,
+    balance: '$4,382.94',
+    gender: 'female'
+  }
+];
+
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
@@ -5,8 +64,9 @@
 const getFirstNames = arr => {
   const userFirstNames = [];
   for (let user of arr) {
-    // Your code goes here ...
+    userFirstNames.push(user.firstName);
   }
+  return userFirstNames
 };
 
 getFirstNames(usersArray);
@@ -18,7 +78,11 @@ getFirstNames(usersArray);
 // ***************************************************************************
 
 const getFullNames = arr => {
-  // Your code goes here ...
+  const userFullNames = [];
+  for (let user of arr) {
+    userFullNames.push(`${user.firstName} ${user.lastName}`);
+  }
+  return userFullNames
 };
 
 getFullNames(usersArray);
@@ -31,7 +95,13 @@ getFullNames(usersArray);
 // ***************************************************************************
 
 const getUsersCreditDetails = arr => {
-  // Your code goes here ...
+  let usersCreditDetails = [];
+  for (let user of arr) {
+    let { firstName, lastName, balance } = user
+    let userDetails = { firstName, lastName, balance };
+    usersCreditDetails.push(userDetails);
+  }
+  return usersCreditDetails
 };
 
 getUsersCreditDetails(usersArray);
@@ -49,7 +119,10 @@ getUsersCreditDetails(usersArray);
 // ***************************************************************************
 
 const genderView = users => {
-  // Your code goes here ...
+  return {
+    femaleUsers: getFullNames(users.filter((user) => { return user.gender === 'female' })),
+    maleUsers: getFullNames(users.filter((user) => { return user.gender === 'male' }))
+  } 
 };
 
 genderView(usersArray);
@@ -66,7 +139,11 @@ genderView(usersArray);
 const data = genderView(usersArray);
 
 const genderCount = data => {
-  // Your code goes here ...
+  let { femaleUsers } = data
+  let { maleUsers } = data
+
+  return `Female: ${femaleUsers.length}
+  Male: ${maleUsers.length}`
 };
 
 genderCount(data);
@@ -79,8 +156,14 @@ genderCount(data);
 // ***************************************************************************
 
 const promo20 = users => {
-  // Your code goes here ...
+  users.forEach((user) => {
+    if (user.balance.replace(/[^0-9.-]+/g,"") > 20000) {
+      console.log(`Dear ${user.firstName}, since your balance is ${user.balance}, you are eligible to apply for this awesome credit card.`) 
+    }
+  })
 };
+
+// promo20(usersArray);
 
 // expected output:
 // Dear Howard, since your balance is $21,307.75, you are eligible to apply for this awesome credit card.
@@ -91,10 +174,24 @@ const promo20 = users => {
 // ***************************************************************************
 
 const addActive = users => {
-  // Your code goes here ...
+  let newArr = [];
+  
+  users.forEach((user) => {
+    newArr.push({ ...user, isActive: true });
+  });
+
+  return newArr
 };
 
-addActive(usersArray);
+//another way of doing this:
+// const addActive = users => {
+//   users.forEach((user) => {
+//     user.isActive = true;
+//   });
+//   return users
+// };
+
+console.log(addActive(usersArray));
 // expected output:
 // [
 //    { firstName: 'Kirby',
