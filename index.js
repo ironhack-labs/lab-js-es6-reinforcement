7313cd31982cbdc704b1d90b19eb174679104a4e
Justine Benevent <justinebnvent@gmail.com>
2020-05-25 19:20:00 +0300

    done

--- index.js
+++ index.js
@@ -1,15 +1,76 @@
+const usersArray = [
+  {
+    firstName: "Kirby",
+    lastName: "Doyle",
+    id: "b71794e5-851e-44b5-9eec-1dd4e897e3b8",
+    isActive: false,
+    balance: "$3,570.06",
+    gender: "male",
+  },
+  {
+    firstName: "Tracie",
+    lastName: "May",
+    id: "1af0e9ee-66fc-4298-b8ce-5d99bcbaac05",
+    isActive: false,
+    balance: "$1,547.73",
+    gender: "female",
+  },
+  {
+    firstName: "Kendra",
+    lastName: "Hines",
+    id: "5e92af3a-b08e-4689-bdeb-3226300470e3",
+    isActive: true,
+    balance: "$12,383.08",
+    gender: "female",
+  },
+  {
+    firstName: "Kinney",
+    lastName: "Howard",
+    id: "0ad2388d-83e1-4831-9cc4-e3581f8edf36",
+    isActive: false,
+    balance: "$3,207.06",
+    gender: "male",
+  },
+  {
+    firstName: "Howard",
+    lastName: "Gilmore",
+    id: "0719205d-c965-44cb-a128-708cf335b26c",
+    isActive: true,
+    balance: "$21,307.75",
+    gender: "male",
+  },
+  {
+    firstName: "Rachelle",
+    lastName: "Schneider",
+    id: "04012184-651b-41eb-9642-d362fedff02f",
+    isActive: true,
+    balance: "$35,121.49",
+    gender: "female",
+  },
+  {
+    firstName: "Lizzie",
+    lastName: "Alford",
+    id: "598ca7ec-888e-494d-ae94-c21ace3ffa52",
+    isActive: false,
+    balance: "$4,382.94",
+    gender: "female",
+  },
+];
+
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
 
-const getFirstNames = arr => {
+const getFirstNames = (arr) => {
   const userFirstNames = [];
   for (let user of arr) {
-    // Your code goes here ...
+    userFirstNames.push(user.firstName);
   }
+  // console.log(userFirstNames);
 };
 
-getFirstNames(usersArray);
+// getFirstNames(usersArray);
+
 // expected output:
 // [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
 
@@ -17,11 +78,16 @@ getFirstNames(usersArray);
 // Iteration 2 - `for...of` loop and ES6 string literals `${}`
 // ***************************************************************************
 
-const getFullNames = arr => {
+const getFullNames = (arr) => {
   // Your code goes here ...
+  const userFullNames = [];
+  for (let user of arr) {
+    userFullNames.push(`${user.firstName} ${user.lastName}`);
+  }
+  console.log(userFullNames);
 };
 
-getFullNames(usersArray);
+// getFullNames(usersArray);
 // expected output:
 // [ 'Kirby Doyle', 'Tracie May', 'Kendra Hines', 'Kinney Howard',
 //   'Howard Gilmore', 'Rachelle Schneider', 'Lizzie Alford' ]
@@ -30,11 +96,18 @@ getFullNames(usersArray);
 // Iteration 3 - ES6 destructuring , for of loop, object literal
 // ***************************************************************************
 
-const getUsersCreditDetails = arr => {
+const getUsersCreditDetails = (arr) => {
   // Your code goes here ...
+  const userCreditDetails = [];
+  for (let user of arr) {
+    let { firstName, lastName, balance } = user;
+    userCreditDetails.push(`${firstName} ${lastName} ${balance}`);
+  }
+  // console.log(userCreditDetails);
 };
 
-getUsersCreditDetails(usersArray);
+// getUsersCreditDetails(usersArray);
+
 // expected output:
 // [ { firstName: 'Kirby', lastName: 'Doyle', balance: '$3,570.06' },
 // { firstName: 'Tracie', lastName: 'May', balance: '$1,547.73' },
@@ -48,11 +121,14 @@ getUsersCreditDetails(usersArray);
 // Iteration 4 - practice `.filter()` method and how to return two elements
 // ***************************************************************************
 
-const genderView = users => {
-  // Your code goes here ...
+const genderView = (users) => {
+  const femaleUsers = users.filter((user) => user.gender === "female");
+  const maleUsers = users.filter((user) => user.gender === "male");
+  return femaleUsers;
 };
 
 genderView(usersArray);
+
 // expected output:
 // {
 //    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
@@ -65,7 +141,7 @@ genderView(usersArray);
 
 const data = genderView(usersArray);
 
-const genderCount = data => {
+const genderCount = (data) => {
   // Your code goes here ...
 };
 
@@ -78,7 +154,7 @@ genderCount(data);
 // Bonus - Iteration 6
 // ***************************************************************************
 
-const promo20 = users => {
+const promo20 = (users) => {
   // Your code goes here ...
 };
 
@@ -90,7 +166,7 @@ const promo20 = users => {
 // Bonus - Iteration 7
 // ***************************************************************************
 
-const addActive = users => {
+const addActive = (users) => {
   // Your code goes here ...
 };
 
