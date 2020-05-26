2e61f3327d88b7522db2aca24e8ae253edae8208
Michelle Lock <michelleloc@gmail.com>
2020-05-26 15:37:27 +0800

    Fix with module.exports and require

--- index.js
+++ index.js
@@ -1,61 +1,4 @@
-const usersArray = [
-  {
-    firstName: 'Kirby',
-    lastName: 'Doyle',
-    id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
-    isActive: false,
-    balance: '$3,570.06',
-    gender: 'male'
-  },
-  {
-    firstName: 'Tracie',
-    lastName: 'May',
-    id: '1af0e9ee-66fc-4298-b8ce-5d99bcbaac05',
-    isActive: false,
-    balance: '$1,547.73',
-    gender: 'female'
-  },
-  {
-    firstName: 'Kendra',
-    lastName: 'Hines',
-    id: '5e92af3a-b08e-4689-bdeb-3226300470e3',
-    isActive: true,
-    balance: '$12,383.08',
-    gender: 'female'
-  },
-  {
-    firstName: 'Kinney',
-    lastName: 'Howard',
-    id: '0ad2388d-83e1-4831-9cc4-e3581f8edf36',
-    isActive: false,
-    balance: '$3,207.06',
-    gender: 'male'
-  },
-  {
-    firstName: 'Howard',
-    lastName: 'Gilmore',
-    id: '0719205d-c965-44cb-a128-708cf335b26c',
-    isActive: true,
-    balance: '$21,307.75',
-    gender: 'male'
-  },
-  {
-    firstName: 'Rachelle',
-    lastName: 'Schneider',
-    id: '04012184-651b-41eb-9642-d362fedff02f',
-    isActive: true,
-    balance: '$35,121.49',
-    gender: 'female'
-  },
-  {
-    firstName: 'Lizzie',
-    lastName: 'Alford',
-    id: '598ca7ec-888e-494d-ae94-c21ace3ffa52',
-    isActive: false,
-    balance: '$4,382.94',
-    gender: 'female'
-  }
-];
+let usersArray = require('./data.js')
 
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
@@ -69,7 +12,7 @@ const getFirstNames = arr => {
   return userFirstNames
 };
 
-getFirstNames(usersArray);
+console.log(getFirstNames(usersArray));
 // expected output:
 // [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
 
@@ -191,7 +134,7 @@ const addActive = users => {
 //   return users
 // };
 
-console.log(addActive(usersArray));
+addActive(usersArray);
 // expected output:
 // [
 //    { firstName: 'Kirby',
