4a9dfc72719e7bae868dda1132af9c117dea057c
gigi-loranca <85176906+gigi-loranca@users.noreply.github.com>
2021-06-21 19:14:32 +0200

    1-4 done

--- index.js
+++ index.js
@@ -1,39 +1,61 @@
+
+​
+let usersArray = require('./data.js')
+// console.log('hey')
+​
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
-
+​
 const getFirstNames = arr => {
   const userFirstNames = [];
   for (let user of arr) {
-    // Your code goes here ...
+    userFirstNames.push(user.firstName)
   }
+  return userFirstNames
+  // console.log(userFirstNames)
+​
 };
-
+​
+​
 getFirstNames(usersArray);
 // expected output:
 // [ 'Kirby', 'Tracie', 'Kendra', 'Kinney', 'Howard', 'Rachelle', 'Lizzie' ]
-
+​
 // ***************************************************************************
 // Iteration 2 - `for...of` loop and ES6 string literals `${}`
 // ***************************************************************************
-
+​
 const getFullNames = arr => {
-  // Your code goes here ...
+  const userFullNames = [];
+  for (let user of arr) {
+    userFullNames.push(`${user.firstName} ${user.lastName}`)
+  }
+  return userFullNames
+  // console.log(userFullNames)
 };
-
+​
 getFullNames(usersArray);
 // expected output:
 // [ 'Kirby Doyle', 'Tracie May', 'Kendra Hines', 'Kinney Howard',
 //   'Howard Gilmore', 'Rachelle Schneider', 'Lizzie Alford' ]
-
+​
 // ***************************************************************************
 // Iteration 3 - ES6 destructuring , for of loop, object literal
 // ***************************************************************************
-
+​
 const getUsersCreditDetails = arr => {
-  // Your code goes here ...
+  let usersCreditDetails = []
+  for (let user of arr) {
+    const { firstName, lastName, balance } = user
+    let userDetails = { firstName, lastName, balance }
+    usersCreditDetails.push(userDetails)
+  }
+  return usersCreditDetails
+  // console.log(userCreditDetails)
 };
-
+// console.log(getUsersCreditDetails(usersArray))
+​
 getUsersCreditDetails(usersArray);
 // expected output:
 // [ { firstName: 'Kirby', lastName: 'Doyle', balance: '$3,570.06' },
@@ -43,57 +65,74 @@ getUsersCreditDetails(usersArray);
 // { firstName: 'Howard', lastName: 'Gilmore', balance: '$21,307.75' },
 // { firstName: 'Rachelle', lastName: 'Schneider', balance: '$35,121.49' },
 // { firstName: 'Lizzie', lastName: 'Alford', balance: '$4,382.94' } ]
-
+​
 // ***************************************************************************
 // Iteration 4 - practice `.filter()` method and how to return two elements
 // ***************************************************************************
-
+​
 const genderView = users => {
-  // Your code goes here ...
+  maleUsers = []
+  femaleUsers = []
+  let males = users.filter(elem => {
+    return elem.gender == 'male'
+  })
+  for (let users of males) {
+    maleUsers.push(`${users.firstName} ${users.lastName}`)
+  }
+​
+  let females = users.filter(elem => {
+    return elem.gender == 'female'
+  })
+  for (let users of females) {
+    femaleUsers.push(`${users.firstName} ${users.lastName}`)
+  }
+  return { femaleUsers, maleUsers }
 };
-
+​
+console.log(genderView(usersArray))
+​
 genderView(usersArray);
 // expected output:
 // {
 //    femaleUsers: [ 'Tracie May', 'Kendra Hines', 'Rachelle Schneider', 'Lizzie Alford' ],
 //    maleUsers: [ 'Kirby Doyle', 'Kinney Howard', 'Howard Gilmore' ]
 // }
-
+​
 // ***************************************************************************
 // Bonus - Iteration 5
 // ***************************************************************************
-
+​
 const data = genderView(usersArray);
-
+​
 const genderCount = data => {
   // Your code goes here ...
 };
-
+​
 genderCount(data);
 // expected output:
 // Female: 4
 // Male: 3
-
+​
 // ***************************************************************************
 // Bonus - Iteration 6
 // ***************************************************************************
-
+​
 const promo20 = users => {
   // Your code goes here ...
 };
-
+​
 // expected output:
 // Dear Howard, since your balance is $21,307.75, you are eligible to apply for this awesome credit card.
 // Dear Rachelle, since your balance is $35,121.49, you are eligible to apply for this awesome credit card.
-
+​
 // ***************************************************************************
 // Bonus - Iteration 7
 // ***************************************************************************
-
+​
 const addActive = users => {
   // Your code goes here ...
 };
-
+​
 addActive(usersArray);
 // expected output:
 // [
